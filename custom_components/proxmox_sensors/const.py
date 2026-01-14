2767e5d44c23dd89e275146ba68d7f02012131d2
TTMaster <95102174+Javisen@users.noreply.github.com>
2026-01-14 10:09:28 +0100

    Update const.py
    Add support token id

--- custom_components/proxmox_sensors/const.py
+++ custom_components/proxmox_sensors/const.py
@@ -1,7 +1,10 @@
 DOMAIN = "proxmox_sensors"
 
 CONF_HOST = "host"
-CONF_USER = "user"
-CONF_PASSWORD = "password"
-CONF_NODE = "node"
+CONF_USER = "user"                # ej: ha@pve
+CONF_TOKEN_ID = "token_id"        # ej: homeassistant
+CONF_TOKEN_SECRET = "token_secret" # el secret generado en Proxmox
+CONF_NODE = "node"                # ej: server
+CONF_PLATFORM_TYPE = "platform_type"  # PVE o PBS
 CONF_SENSORS = "sensors"
+
